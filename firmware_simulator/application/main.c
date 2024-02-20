2093c97060237b7606faca45dca5729dc5c6b3b8
Jason Long <jason.long@engenuics.com>
2024-02-20 07:57:28 -0700

    WIP

--- firmware_simulator/application/main.c
+++ firmware_simulator/application/main.c
@@ -29,6 +29,79 @@ Main Program
 
 void main(void)
 {
+  struct UserApp1_asLedMatrixColors
+  {
+    u8 u8Red;
+    u8 u8Grn;
+    u8 u8Blu;
+  };
+  
+  struct UserApp1_asLedMatrixColors asLedMatrixColors [20];
+  
+  u8 u8BitMask = 0x80;        /* Start with MSB mask for a single bit */
+  u8 *pu8BytePointer = &asLedMatrixColors[0].u8Red;    /* Starting address for access pointer */
+  
+  asLedMatrixColors[0].u8Red = 0x9a;
+  asLedMatrixColors[0].u8Grn = 0x9a;
+  asLedMatrixColors[0].u8Blu = 0x9a;
+  asLedMatrixColors[1].u8Red = 0x9a;
+  asLedMatrixColors[1].u8Grn = 0x9a;
+  asLedMatrixColors[1].u8Blu = 0x9a;
+  
+  for(u16 i = 0; i < 60; i++)                                  // 54, 114: 5 instruction cycles
+  {
+    if(u8BitMask & *pu8BytePointer)                            // 59, 122: 4 instruction cycles
+    { 
+      /* One-pulse is 900ns high then 300ns low */
+      AT91C_BASE_PIOA->PIO_SODR = 0x01;                        // 63 need this 120, 177, 234: 4 instruction cycles, pin update on 3rd
+//      for(u8 j = 0; j < 10; j++);                              // 54, 209: j x 8 instruction cycles + x
+      if(u8BitMask == 0x01)                                      // 67: 7 instruction cycles (else)
+      {
+        u8BitMask = 0x80;                                        // 188, 343: 5 instruction cycles
+        pu8BytePointer++;
+      }
+      else
+      {
+        u8BitMask >>= u8BitMask;                                 // 74: 4 instruction cycles
+        asm("nop");                                              // 78: instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+      }
+      for(u8 j = 0; j < 2; j++);                               // 82: 24 instruction cycles
+      asm("nop");                                              // 106: 1 instruction cycle
+      AT91C_BASE_PIOA->PIO_CODR = 0x01;                       // Need this to be 107: 4 instruction cycles, pin update on 3rd
+//      for(u8 j = 2; j > 0; j--);                               // 111: j x 12 instruction cycles + x
+//      asm("nop");                                              // 135: 1 instruction cycle
+    } /* end if 1-bit */
+    else
+    {
+      /* Zero-pulse is 300ns high then 900ns low */
+      AT91C_BASE_PIOA->PIO_SODR = 0x01;                        // 63, 129 need this 120: 4 instruction cycles, pin update on 3rd
+      for(u8 j = 0; j < 1; j++);                               // 82: 12 instruction cycles
+      asm("nop");                                              // 106: 1 instruction cycle
+      asm("nop");                                              // 106: 1 instruction cycle
+      AT91C_BASE_PIOA->PIO_CODR = 0x01;                       // Need this to be 107: 4 instruction cycles, pin update on 3rd
+      if(u8BitMask == 0x01)                                      // 67: 7 instruction cycles (else)
+      {
+        u8BitMask = 0x80;                                        // 188, 343: 5 instruction cycles
+        pu8BytePointer++;
+      }
+      else
+      {
+        u8BitMask >>= u8BitMask;                                 // 74: 4 instruction cycles
+        asm("nop");                                              // 78: instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+        asm("nop");                                              // 1 instruction cycle
+      }
+      
+      for(u8 j = 0; j < 1; j++);                                 // 82: 12 instruction cycles
+
+    } /* end if 0-bit */
+    
+    /* Shift mask and reset/move pointer if necessary */
+  }
 
   while(1)
   {
